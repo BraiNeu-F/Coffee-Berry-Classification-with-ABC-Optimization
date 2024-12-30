b81b622223f7edde5165e6f3bb912d2b2aacabdd
Faiz <97728873+BraiNeu-F@users.noreply.github.com>
2024-12-30 08:40:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('./coffee_quality_model.h5')
+    model = tf.keras.models.load_model('coffee_quality_model.h5')
     return model
 
 model = load_model()
